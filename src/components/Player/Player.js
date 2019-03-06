363275b07edbfc3fa3759579307bf917a9cc24c0
daffpunks <sawavas2@gmail.com>
2019-03-06 23:40:47 +0300

    feat(Player): add timeline

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -6,7 +6,10 @@ import './Player.scss';
 class Player extends React.Component {
 
     state = {
-        player: null
+        transitionX: 0,
+        initialX: 0,
+        maxX: 0,
+        percent: 0
     };
 
 
@@ -18,21 +21,96 @@ class Player extends React.Component {
         this.player = player;
     };
 
+    progressRef = progress => {
+        const initial = progress.getBoundingClientRect();
+
+        this.setState({
+            initialX: initial.x,
+            transitionX: initial.x,
+            maxX: initial.width
+        });
+    };
+
+    onMouseDown = (e) => {
+        this.setState(
+            {
+                transitionX: e.clientX
+            },
+            () => {
+                document.addEventListener('mousemove', this.onMouseMove);
+                document.addEventListener('mouseup', this.onMouseUp);
+            }
+        );
+    };
+
+    onMouseUp = () => {
+        document.removeEventListener('mousemove', this.onMouseMove);
+        document.removeEventListener('mouseup', this.onMouseUp);
+
+        this.seek();
+    };
+
+    onMouseMove = (e) => {
+        const {initialX, maxX} = this.state;
+        let transition = e.clientX;
+
+        if (e.clientX - initialX < 0) {
+            transition = initialX;
+        }
+
+        if (e.clientX - initialX > maxX) {
+            transition = initialX + maxX;
+        }
+
+        this.setState({transitionX: transition});
+    };
+
+    onLineClick = async (e) => {
+        await this.setState({transitionX: e.clientX});
+
+        this.seek();
+    };
+
+    seek = async () => {
+        const {initialX, transitionX, maxX} = this.state;
+
+        const lengthPercent = (transitionX - initialX) / (maxX / 100) / 100;
+
+        this.setState({percent: lengthPercent});
+
+        this.player.seekTo(parseFloat(lengthPercent))
+    }
+
     render() {
+        const {transitionX, initialX, percent} = this.state;
+
         return (
             <div className="player">
-                <ReactPlayer
-                    ref={this.ref}
-                    playing={true}
-                    url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
-                    width="100%"
-                    height="100%"
-                    volume={.01}
-                    onSeek={this.onSeek}
-                />
-                <input type="range" step="any" min="0" max="1"
-                       onMouseUp={this.onSeekMouseUp}
-                />
+                <div className="player__player">
+                    <ReactPlayer
+                        ref={this.ref}
+                        playing={true}
+                        url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+                        width="100%"
+                        height="100%"
+                        volume={.01}
+                        onSeek={this.onSeek}
+                        controls={true}
+                    />
+                    <div></div>
+                </div>
+                <div className='player__controls'>
+                    <div className="player__progress"
+                         ref={this.progressRef}
+                         onClick={this.onLineClick}
+                    >
+                        <div className="player__progress-point"
+                             style={{transform: `translateX(${transitionX - initialX}px)`}}
+                             onMouseDown={this.onMouseDown}
+                        />
+                        <div className="player__progress-inside" style={{width: `${percent * 100}%`}}/>
+                    </div>
+                </div>
             </div>
         );
     }
